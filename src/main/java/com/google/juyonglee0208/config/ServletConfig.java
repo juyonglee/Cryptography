4568c193cd25b8aaf1b946cdfbc5668071af5143
juyonglee <juyonglee0208@gmail.com>
2020-04-08 18:29:29 +0900

    Servlet을 위한 설정 Class 작성

---
+++ src/main/java/com/google/juyonglee0208/config/ServletConfig.java
@@ -0,0 +1,30 @@
+package com.google.juyonglee0208.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.google.juyonglee0208"})
+public class ServletConfig implements WebMvcConfigurer {
+	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		InternalResourceViewResolver bean = new InternalResourceViewResolver();
+		bean.setViewClass(JstlView.class);
+		bean.setPrefix("/WEB-INF/views/");
+		bean.setSuffix(".jsp");
+		registry.viewResolver(bean);
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// TODO Auto-generated method stub
+		WebMvcConfigurer.super.addResourceHandlers(registry);
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+}
