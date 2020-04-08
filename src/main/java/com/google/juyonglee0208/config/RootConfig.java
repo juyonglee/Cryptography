481fd1a8dae36ea6d1f7f0daf71c33534ed75b3c
juyonglee <juyonglee0208@gmail.com>
2020-04-08 18:29:03 +0900

    Spring Container를 위한 설정 Class 작성

--- src/main/java/com/google/juyonglee0208/config/RootConfig.java
+++ src/main/java/com/google/juyonglee0208/config/RootConfig.java
@@ -1,5 +1,35 @@
 package com.google.juyonglee0208.config;
 
-public class RootConfig {
+import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@ComponentScan(basePackages = {"com.google.juyonglee0208"})
+public class RootConfig {
+	
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName("net.sf.log4jdbc.DriverSpy");
+		hikariConfig.setJdbcUrl("jdbc:log4:mysql://localhost");
+		hikariConfig.setUsername("root");
+		hikariConfig.setPassword("0000");	
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+		return dataSource;
+	}
+	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception {
+		SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+		sqlSessionFactory.setDataSource(dataSource());
+		return sqlSessionFactory.getObject();
+	}
 }
